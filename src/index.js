9eddfdad23971e3e12cb9477ed9714349f9fc4ff
nnorbert19 <norb0919@gmail.com>
2022-12-09 16:22:43 +0100

    .

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ContentfulProvider } from "./ContentfulContext";
 import Home from "./Pages/Home";
 import Layout from "./Pages/Layout";
@@ -14,7 +14,8 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route path="*" index element={<Home />} />
+            <Route index element={<Home />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
           </Route>
         </Routes>
       </BrowserRouter>
